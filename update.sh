e9d1096a3c5960174b1509320a112133731cd855
xCuri0 <zkqri0@gmail.com>
2019-10-05 16:32:01 +0530

    fix strcoll
    copying it before other benchmark programs seems to work

--- update.sh
+++ update.sh
@@ -10,6 +10,7 @@ mkdir -p wcsmbs
 mkdir -p sysdeps/generic
 
 cp -a ${GLIBC}/benchtests/!(Makefile) .
+cp ${GLIBC}/benchtests/*.c .
 cp ${GLIBC}/support/{support_test_main.c,test-driver.c,set_fortify_handler.c,ignore_stderr.c,xpthread_create.c,xpthread_check_return.c,check.c,support_record_failure.c,write_message.c,xpthread_attr_setstacksize.c,xpthread_attr_setguardsize.c,xpthread_join.c,xpthread_attr_init.c,check.h,support.h,temp_file-internal.h,temp_file.h,test-driver.h,xsignal.h,xthread.h} support
 cp ${GLIBC}/sysdeps/generic/{_itoa.h,hp-timing-common.h,hp-timing.h,memcopy.h,pagecopy.h} sysdeps/generic
 cp ${GLIBC}/string/{memcpy.c,memmove.c,memset.c,str-two-way.h,strcasestr.c,strlen.c,strnlen.c,wordcopy.c} string
